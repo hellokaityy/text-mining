3dc9423698a08a8ffe2f66d949e9b5b6239dd773
hellokaityy <70179320+hellokaityy@users.noreply.github.com>
2021-11-03 23:43:59 -0400

    finished project and reflection

--- project.py
+++ project.py
@@ -1,18 +1,53 @@
 from newspaper import Article
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
-
+import nltk
+import string
+nltk.download('vader_lexicon')
 url1 = 'https://www.cbsnews.com/live-updates/trump-us-capitol-secured-dc-protest/'
 articleCBS = Article(url1)
-url2 = 'https://www.foxnews.com/politics/how-wednesdays-capitol-riot-come-to-fruition'
-articleFox = Article(url2)
 articleCBS.download()
 articleCBS.parse()
+CBStext = articleCBS.text
+url2 = 'https://www.foxnews.com/politics/how-wednesdays-capitol-riot-come-to-fruition'
+articleFox = Article(url2)
+articleFox.download()
+articleFox.parse()
+Foxtext = articleFox.text
+
+stopWords = ['a','the','and','an','this','how','why','would','could','For','The']
+
+def analyze(articleText, name):
+    score = SentimentIntensityAnalyzer().polarity_scores(articleText)
+    print(f'The news for {name} score is: {score}')
+
+    wordlist = articleText.split()
+    strippables = string.punctuation + string.whitespace
+
+    wordfreq = []
+
+    for w in wordlist:
+        w = w.strip(strippables)
+        w = w.lower()
+        if w not in stopWords:
+            wordfreq.append(wordlist.count(w))
+    
+    totalFreq= str(list(zip(wordlist, wordfreq)))
+
+    return totalFreq
 
 
 
-sentence = 'Software Design is my favorite class because learning Python is so cool!'
-score = SentimentIntensityAnalyzer().polarity_scores(sentence)
-print(score)
+def sortlist(totalFreq):
+    sorted = [(totalFreq[1], key) for key in totalFreq]
+    sorted.sort()
 
+    return sorted
+
+
+
+print(analyze(Foxtext, 'Fox'))
+print(analyze(CBStext, 'CBS'))
+# print(analyzeFox())
+# print(sortlist(totalFreqFox))
+#print(removeStopWords(totalFreqCBS, stopWords))
 
